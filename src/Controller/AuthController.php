b3a8078cc0e162ef5b7894e4d5e77c7713d85214
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-04-09 22:22:47 +0400

    Mettre à jour les routes, composer.json, lexik_jwt_authentication.yaml, AuthController.php, bundles.php, AppFixtures.php, .env, symfony.lock, UserFixtures.php, et security.yaml

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api')]
 class AuthController extends AbstractController
 {
     #[Route('/register', name: 'app_register', methods: ['POST'])]
--- tests/Controller/AuthControllerTest.php
+++ tests/Controller/AuthControllerTest.php
@@ -9,7 +9,7 @@ class AuthControllerTest extends WebTestCase
     public function testRegister(): void
     {
         $client = static::createClient();
-        $client->request('POST', '/register', [], [], [], json_encode([
+        $client->request('POST', '/api/register', [], [], [], json_encode([
             'email' => 'test@gmail.com',
 
             'password' => 'password',
@@ -19,5 +19,21 @@ class AuthControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(201);
         $this->assertJsonStringEqualsJsonString('{"message":"User created"}', $client->getResponse()->getContent());
+    }
+
+    public function testLogin(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/login_check', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
+            'username' => 'testAdmin',
+            'password' => 'password'
+        ]));
+
+        $response = $client->getResponse();
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertArrayHasKey('token', $responseData);
     }
 }
